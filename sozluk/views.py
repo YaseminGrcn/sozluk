f2235c3188bc7c815f00bf059719806689425c51
YaseminGrcn <yasmin.06.gurcan@gmail.com>
2017-06-03 12:31:46 +0300

    Added entry list

--- sozluk/views.py
+++ sozluk/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.views.generic import View
+from sozluk.models import Entry
 
-# Create your views here.
+
+def post_list(request):
+    template_path = 'sozluk/entry_list.html'
+    context = {}
+    all_entry = Entry.objects.all()
+    entry_list = []
+    if all_entry:
+        for entry in all_entry:
+            entry_list.append(entry)
+    context['entry_body'] = entry_list
+    return render(request, template_path, context)
