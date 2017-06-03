bc6fdca14c11d6b635bf9137463ecc6c7a9ad1c4
YaseminGrcn <yasmin.06.gurcan@gmail.com>
2017-06-03 14:35:52 +0300

    Added base html

--- sozluk/views.py
+++ sozluk/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, render_to_response
 from django.views.generic import View
-from sozluk.models import Entry
+from sozluk.models import Entry, Post
 
 
 def post_list(request):
+
     template_path = 'sozluk/entry_list.html'
     context = {}
     all_entry = Entry.objects.all()
@@ -11,5 +12,5 @@ def post_list(request):
     if all_entry:
         for entry in all_entry:
             entry_list.append(entry)
-    context['entry_body'] = entry_list
+    context['entry_list']  = entry_list
     return render(request, template_path, context)
